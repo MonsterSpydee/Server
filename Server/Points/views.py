ee647097a97820116d0709200fdd6d2979ba570d
Rajat <rajatbhatta6399@gmail.com>
2018-12-02 02:19:03 +0530

    resolve points bug

--- Server/Points/views.py
+++ Server/Points/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from .models import POINTS
 from .serializers import POINTSSerializer
 import json
+from rest_framework import status
 
 class pointsList(APIView):
 	def get(self,request):
@@ -13,14 +14,15 @@ class pointsList(APIView):
 		serializer = POINTSSerializer(points, many = True)
 		return Response(serializer.data)
 
-	def post(self,request,formt ='json'):
+	def post(self,request,format ='json'):
 		serializer = POINTSSerializer(data = request.data)
 		if serializer.is_valid():
 			new_point = POINTS(notes_upvote = request.data["notes_upvote"], 
 				notes_downvote = request.data["notes_downvote"], assignments_upvote = request.data["assignments_upvote"], assignments_downvote = request.data["assignments_downvote"] )
 			new_point.save()
-			print(new_point)
-			return Response(json, status=status.HTTP_201_CREATED)
+			#print(new_point)
+			js = json.dumps({"status" : "true", "msg" : "Points object created"})
+			return Response(js, status=status.HTTP_201_CREATED)
 		else:
 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 # Create your views here.
