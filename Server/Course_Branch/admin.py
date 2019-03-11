e4ca9cc311b493b7de34292ab975c843f14fdee6
Rajat <rajatbhatta6399@gmail.com>
2019-03-11 19:05:44 +0530

    add link to admin page

--- Server/Course_Branch/admin.py
+++ Server/Course_Branch/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import COURSE,BRANCH
+
+admin.site.register(COURSE)
+admin.site.register(BRANCH)
--- Server/Post/admin.py
+++ Server/Post/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import POST, VOTES_STATUS
+
+admin.site.register(POST)
+admin.site.register(VOTES_STATUS)
